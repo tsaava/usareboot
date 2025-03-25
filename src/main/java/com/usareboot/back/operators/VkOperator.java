01a0b1bd9ef827108d9a4de861de74be72a5ea94
Vik <tsaawa2019@gmail.com>
2025-03-25 18:12:08 +0300

    подтягивание правильного токена для загрузки фото в альбом

--- src/main/java/com/usareboot/back/operators/VkOperator.java
+++ src/main/java/com/usareboot/back/operators/VkOperator.java
@@ -44,7 +44,7 @@ public class VkOperator {
     private final VkApiClient vkApiClient;
 
     public String getCommentPhotoVk(long photo_id) throws IOException {
-        var accessToken = commonOperator.getTokenGroup(clientId).orElse(null);
+        var accessToken = commonOperator.getTokenClient(standaloneId).orElse(null);
         final CloseableHttpClient httpclient = HttpClients.createDefault();
         final HttpPost httpPost = new HttpPost("https://api.vk.com/method/photos.getById");
         final List<NameValuePair> params = new ArrayList<>();
