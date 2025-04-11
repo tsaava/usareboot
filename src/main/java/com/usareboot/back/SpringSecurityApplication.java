8069cb9473819f16a526ae94a88645327571c436
Vik <tsaawa2019@gmail.com>
2025-04-11 14:54:42 +0300

    изменен конфиг yaml

--- src/main/java/com/usareboot/back/SpringSecurityApplication.java
+++ src/main/java/com/usareboot/back/SpringSecurityApplication.java
@@ -12,9 +12,7 @@ import java.util.Collections;
 
 @SpringBootApplication
 @EnableFeignClients
-public class SpringSecurityApplication implements CommandLineRunner {
-    @Autowired
-    private YAMLConfig myConfig;
+public class SpringSecurityApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SpringSecurityApplication.class);
         app.setDefaultProperties(Collections.singletonMap("server.port", 9000));
@@ -24,9 +22,5 @@ public class SpringSecurityApplication implements CommandLineRunner {
     public void init() {
         // Регистрация WebP плагина для ImageIO
         ImageIO.scanForPlugins();
-    }
-    public void run(String... args) throws Exception {
-//        System.out.println("using environment: " + myConfig.getEnvironment());
-//        System.out.println("name: " + myConfig.getName());
     }
 }
